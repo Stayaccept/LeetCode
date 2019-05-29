868a43c16ab4609e92776082598822a34735abcb
Stayaccept <2427751718@qq.com>
2019-05-29 19:35:45 +0800

    Update 830. 较大分组的位置.cpp

--- 830. 较大分组的位置.cpp
+++ 830. 较大分组的位置.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string S) {
-        int i,l,r,n,u;
-        vector<int> tmp;
-        vector<vector<int> > ans;
-        n=S.size();
-        for(i=0;i<n;i++){
-            if(tmp.size()==0)
-            tmp.push_back(S[i]);
-            else if(S[i]==S[i-1])
-            tmp.push_back(S[i]);
-            else{
-                u=tmp.size();
+         int i,j,n;
+         vector<int> tmp;
+         vector<vector<int> > ans;
+         n=S.size();
+         i=0;
+         while(i<n){
+            j=i;
+            while(j+1<n&&S[j+1]==S[j])
+            j++;
+            if(j-i+1>=3){
                 tmp.clear();
-                if(u>=3){
-                    tmp.push_back(i-u);
-                    tmp.push_back(i-1);
-                    ans.push_back(tmp);
-                }
-                tmp.clear();
-                tmp.push_back(S[i]);
+                tmp.push_back(i);
+                tmp.push_back(j);
+                ans.push_back(tmp);
             }
-        }
-        u=tmp.size();
-        if(u>=3){
-            tmp.clear();
-            tmp.push_back(n-u);
-            tmp.push_back(n-1);
-            ans.push_back(tmp);
-        }
-        return ans;
+            i=j+1;
+         }
+         return ans;
     }
 };
